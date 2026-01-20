cac1abb711ab45ce83b8740a5628cc5be9bd1e1c
JEBA0331 <jebakirubagarc@gmail.com>
2026-01-20 12:24:46 +0530

    Initial commit - Python internship tasks

---
+++ basicchatbot.py
@@ -0,0 +1,15 @@
+print("🤖 Chatbot Started! Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ").lower()
+
+    if user_input == "hello":
+        print("Bot: Hi!")
+    elif user_input == "how are you":
+        print("Bot: I'm fine, thank you!")
+    elif user_input == "bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: Sorry, I don't understand.")
+ 
